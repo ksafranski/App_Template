355184c359dc40fd05f980d6afa7191a80d83f06
fluidbyte <kent@fluidbyte.net>
2012-11-10 19:35:50 +0000

    Fix requirejs pathing

--- js/init.js
+++ js/init.js
@@ -7,9 +7,9 @@ var scripts = [
     "//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.2/underscore-min.js",
     "//cdnjs.cloudflare.com/ajax/libs/backbone.js/0.9.2/backbone-min.js",
     '//cdnjs.cloudflare.com/ajax/libs/css3finalize/3.1/jquery.css3finalize.min.js',
-    'js/placeholder.js',
-    'js/modal.js',
-    'js/alert.js'
+    'placeholder',
+    'modal',
+    'alert'
 ];
 
 require(scripts, function($) {
